d107fc82891fcd7037cc83638c0272818ff77640
digi-monkey <digimonkey@protonmail.com>
2024-01-10 16:15:06 +0800

    refactor: smaller binary

--- script/src/lib.rs
+++ script/src/lib.rs
@@ -1,7 +1,6 @@
-extern crate whatlang;
-
 use js_sys::Reflect;
 use wasm_bindgen::prelude::*;
+use whatlang::{detect_script, Script};
 
 #[wasm_bindgen]
 pub fn pre_validate(){
@@ -13,8 +12,8 @@ pub fn is_valid_event(event: JsValue) -> bool {
     if let Some(obj) = event.dyn_ref::<js_sys::Object>() {
         if let Ok(content) = Reflect::get(obj, &JsValue::from_str("content")) {
             if let Some(content) = content.as_string() {
-                let info = whatlang::detect(content.as_str()).unwrap();
-                if info.lang().eq(&whatlang::Lang::Jpn) {
+                let info = detect_script(content.as_str()).unwrap();
+                if info.eq(&Script::Hiragana) || info.eq(&Script::Katakana){
                     return true;
                 }
            }
